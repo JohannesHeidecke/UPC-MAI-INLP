862e7f4eaa7845c33abf45a1346d6334c585ff92
Johannes Heidecke <johannesheidecke@Johanness-MacBook.local>
2016-09-22 14:04:41 +0200

    Simple test using NLTK to tokenize a sentence

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # UPC-MAI-INLP
+requirements: 
+* Python
+* NLTK (Natrual Language Toolkit) for Python (`pip install nltk`)
+
+
+
---
+++ test.py
@@ -0,0 +1,7 @@
+import nltk
+from nltk import word_tokenize
+
+sentence = "I am a test"
+tokens = word_tokenize(sentence)
+
+print(tokens)
